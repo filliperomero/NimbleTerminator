640389077576ebf9b741add6340fc7ccc6ed7a76
Fillipe Romero <fillipe.romero@gmail.com>
2022-05-07 17:50:32 -0600

    chore: add jog start/stop animation blendspace

--- Source/NimbleTerminator/Character/NimbleTerminatorAnimInstance.cpp
+++ Source/NimbleTerminator/Character/NimbleTerminatorAnimInstance.cpp
@@ -5,6 +5,7 @@
 
 #include "NimbleTerminatorCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UNimbleTerminatorAnimInstance::UpdateAnimationProperties(float DeltaTime)
 {
@@ -24,6 +25,14 @@ void UNimbleTerminatorAnimInstance::UpdateAnimationProperties(float DeltaTime)
 		NimbleTerminatorCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ?
 			true :
 			false;
+
+	const FRotator AimRotation = NimbleTerminatorCharacter->GetBaseAimRotation();
+	const FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(NimbleTerminatorCharacter->GetVelocity());
+	
+	MovementOffsetYaw = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation).Yaw;
+
+	if (NimbleTerminatorCharacter->GetVelocity().Size() > 0.f)
+		LastMovementOffsetYaw = MovementOffsetYaw;
 }
 
 void UNimbleTerminatorAnimInstance::NativeInitializeAnimation()
--- Source/NimbleTerminator/Character/NimbleTerminatorAnimInstance.h
+++ Source/NimbleTerminator/Character/NimbleTerminatorAnimInstance.h
@@ -32,5 +32,12 @@ private:
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bIsAccelerating;
-	
+
+	// Offset yaw used for strafing
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float MovementOffsetYaw;
+
+	// Offset yaw the frame we stopped moving
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float LastMovementOffsetYaw;
 };
